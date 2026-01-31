4c368ac4e8dd6ed3574bd71f5e176160db3498ba
Daniil K <danilkarachev234@mail.ru>
2026-01-31 22:34:48 +0500

    added animation

--- src/pages/PermissionPage.tsx
+++ src/pages/PermissionPage.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import LightRotate from "../entities/LightRotate";
 import BackgroundWithStars from "../shared/BackgroundWithStars";
 
@@ -25,25 +26,45 @@ function PermissionPage() {
     // <section className="h-screen  bg-black p-4 flex flex-col items-center justify-center text-white">
     <BackgroundWithStars>
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="bebas uppercase text-[2.25rem]">permission Request </h1>
-        <div className="relative ">
+        <motion.h1
+          initial={{ opacity: 0, transform: "scale(0)" }}
+          animate={{ opacity: 1, transform: "scale(1)" }}
+          transition={{ duration: 0.5 }}
+          className="bebas uppercase text-[2.25rem]"
+        >
+          permission Request
+        </motion.h1>
+        <motion.div
+          initial={{ opacity: 0, transform: "scale(0)" }}
+          animate={{ opacity: 1, transform: "scale(1)" }}
+          transition={{ duration: 1 }}
+          className="relative "
+        >
           <LightRotate src="/light/purple.png" />
           <img
             src="/lock.png"
             alt="lock"
             className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]"
           />
-        </div>
-        <p className="inter mb-[2.9rem] max-w-[275px] text-center text-[12px] font-medium">
+        </motion.div>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5 }}
+          className="inter mb-[2.9rem] max-w-[275px] text-center text-[12px] font-medium"
+        >
           For the proper functioning of the application, access to phone
           orientation settings is required
-        </p>
-        <button
+        </motion.p>
+        <motion.button
           onClick={requestMotionPermission}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
           className="bebas text-[2.5rem] max-w-[315px] px-6 py-5.25 btnPurple leading-none rounded-3xl cursor-pointer"
         >
           Request orientation permission
-        </button>
+        </motion.button>
       </div>
     </BackgroundWithStars>
     // </section>
